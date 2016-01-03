ed92ca1688b1637738213410296bf47c42c3df98
leader22 <y.sugiura.0316@gmail.com>
2016-01-03 17:47:05 +0900

    base

---
+++ src/app.js
@@ -0,0 +1,10 @@
+'use strict';
+const React    = require('react');
+const ReactDOM = require('react-dom');
+
+const TodoApp  = require('./components/todo-app.jsx');
+
+ReactDOM.render(
+  <TodoApp />,
+  document.getElementById('jsApp')
+);
---
+++ src/components/todo-app.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+const TodoApp = React.createClass({
+  render: function() {
+    return (
+      <div className="commentBox">
+        Hello, world! I am a CommentBox.
+      </div>
+    );
+  }
+});
+
+module.exports = TodoApp;
